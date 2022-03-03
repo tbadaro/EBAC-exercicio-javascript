3c514c7499883a71d14e2692dc9ca214f021b7f7
tbadaro <49101906+tbadaro@users.noreply.github.com>
2022-03-03 01:18:48 -0300

    Delete soma_multiplos.test.js

--- soma_multiplos.test.js
+++
@@ -1,9 +0,0 @@
-const { somarMultiplosAteLimite } = require('./soma_multiplos')
-describe('Soma os múltiplos de dois números até o valor limite', () => {
-    it('Soma multiplos', () => {
-        expect(somarMultiplosAteLimite(5,7,1000)).toBe(171571)
-        expect(somarMultiplosAteLimite(2,3,10)).toBe(48)
-        expect(somarMultiplosAteLimite(3,2,5)).toBe(9)
-    });
-    
-});
