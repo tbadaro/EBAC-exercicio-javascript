0da54b87f9f108bc9cb7110347515d9c0b8c169b
tbadaro <49101906+tbadaro@users.noreply.github.com>
2022-03-03 01:18:30 -0300

    Delete maior_menor.test.js

--- maior_menor.test.js
+++
@@ -1,8 +0,0 @@
-const { maiorMenor } = require("./maior_menor");
-
-describe('Seleciona o menor e o maior valor de um array', () => {
-    it('Maior e menor valor', () => {
-        expect(maiorMenor([98459, 48759589, 5892349240, 32, 95839234, 39835731, 49320523, 3043752])).toBe(`Maior = ${5892349240} Menor = ${32}`)
-        expect(maiorMenor([3, 4, 19, 35])).toBe(`Maior = ${35} Menor = ${3}`)      
-    });    
-});
