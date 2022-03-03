60de1f204822b6eb4c680b00a04009796c760aea
Tadeu Badaró <t.badaro@gmail.com>
2022-03-03 01:37:41 -0300

    mudanças nos arquivos das funcoes

--- maior_menor.js
+++ maior_menor.js
@@ -1,3 +1,23 @@
+let numeros =[98459, 48759589, 5892349240, 32, 95839234, 39835731, 49320523, 3043752];
+a = -Infinity;
+b = Infinity;
+
+numeros.forEach((element) => {
+    if (a < element) {
+        a = element
+    }
+});
+
+console.log('maior valor: '+ a)
+
+numeros.forEach((element) => {
+    if (b > element) {
+        b = element
+    }
+});
+
+console.log('menor valor: '+ b)
+
 function maiorMenor (array){
 
     let numeros = array
@@ -20,22 +40,3 @@ function maiorMenor (array){
 
 module.exports = { maiorMenor }
 
-// let numeros =[98459, 48759589, 5892349240, 32, 95839234, 39835731, 49320523, 3043752];
-// a = -Infinity;
-// b = Infinity;
-
-// numeros.forEach((element) => {
-//     if (a < element) {
-//         a = element
-//     }
-// });
-
-// console.log('maior valor: '+ a)
-
-// numeros.forEach((element) => {
-//     if (b > element) {
-//         b = element
-//     }
-// });
-
-// console.log('menor valor: '+ b)
